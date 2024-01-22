311f64702e461e5cef7cf177aa7fb43e6fd67f18
Jessie Kirk <thejessiekirk@proton.me>
2024-01-22 15:34:15 +0000

    Initial commit

---
+++ neue/install/aarch64-unknown-linux-gnu/lib/gcc/aarch64-unknown-linux-gnu/13.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./../../../src/gcc-13.2.0/configure --prefix=/usr/skyhammer/neue/install/aarch64-unknown-linux-gnu --target=aarch64-unknown-linux-gnu --disable-multilib --enable-languages=ada,c,c++,d,fortran,go,lto,m2,objc,obj-c++ --enable-threads=posix";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
