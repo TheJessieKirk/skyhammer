e0e9d4ab92e5e1e633585d00e03c3283230bdc15
Jessie Kirk <thejessiekirk@proton.me>
2024-01-22 16:30:13 +0000

    Initial commit

---
+++ neue/install/x86_64-w64-mingw32/lib/gcc/x86_64-w64-mingw32/13.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "./../../../src/gcc-13.2.0/configure --prefix=/usr/skyhammer/neue/install/x86_64-w64-mingw32 --target=x86_64-w64-mingw32 --disable-multilib --enable-languages=c,c++,d,fortran,lto,m2,objc,obj-c++ --enable-threads=posix";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
