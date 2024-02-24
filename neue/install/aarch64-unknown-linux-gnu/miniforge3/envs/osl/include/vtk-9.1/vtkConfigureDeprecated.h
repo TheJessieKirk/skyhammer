7ae71a2150dbc5dae1920c92e8605ef769f9d157
Jessie Kirk <thejessiekirk@proton.me>
2024-02-24 10:21:09 +0000

    Added conda-forge, osl, tl and cloog.

---
+++ neue/install/aarch64-unknown-linux-gnu/miniforge3/envs/osl/include/vtk-9.1/vtkConfigureDeprecated.h
@@ -0,0 +1,24 @@
+/*=========================================================================
+
+  Program:   Visualization Toolkit
+  Module:    vtkConfigureDeprecated.h.in
+
+  Copyright (c) Ken Martin, Will Schroeder, Bill Lorensen
+  All rights reserved.
+  See Copyright.txt or http://www.kitware.com/Copyright.htm for details.
+
+     This software is distributed WITHOUT ANY WARRANTY; without even
+     the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+     PURPOSE.  See the above copyright notice for more information.
+
+=========================================================================*/
+#ifndef vtkConfigureDeprecated_h
+#define vtkConfigureDeprecated_h
+
+/* Whether the target platform supports shared libraries.  */
+/* #undef VTK_TARGET_SUPPORTS_SHARED_LIBS */
+
+/* C++ compiler used.  */
+#define VTK_CXX_COMPILER "/home/conda/feedstock_root/build_artifacts/vtk_1636573297814/_build_env/bin/aarch64-conda-linux-gnu-c++"
+
+#endif
