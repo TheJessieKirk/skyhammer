8475c5d276f33a4a7761093e6544b576b61d55ab
Jessie Kirk <thejessiekirk@proton.me>
2024-09-05 11:44:23 +0000

    Initial commit

---
+++ aarch64-unknown-linux-gnu/lib/gcc/aarch64-unknown-linux-gnu/14.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../../src/gcc-14.2.0/configure --build=aarch64-unknown-linux-gnu --enable-languages=ada,c,c++,d,fortran,go,lto,m2,objc,obj-c++ --prefix=/opt/skyhammer/aarch64-unknown-linux-gnu --enable-shared --enable-static --with-gmp=/opt/skyhammer/aarch64-unknown-linux-gnu --with-mpfr=/opt/skyhammer/aarch64-unknown-linux-gnu --with-mpc=/opt/skyhammer/aarch64-unknown-linux-gnu PATH=/opt/skyhammer/aarch64-unknown-linux-gnu/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { NULL, NULL} };
