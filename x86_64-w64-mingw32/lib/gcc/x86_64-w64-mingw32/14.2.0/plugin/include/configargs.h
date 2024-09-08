085fa56fb1c7110447cde62485c7c58566e44dd0
Jessie Kirk <thejessiekirk@proton.me>
2024-09-08 09:23:34 +0000

    Added x86_64-w64-mingw32 target support.

---
+++ x86_64-w64-mingw32/lib/gcc/x86_64-w64-mingw32/14.2.0/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../../../src/gcc-14.2.0/configure --build=aarch64-unknown-linux-gnu --target=x86_64-w64-mingw32 --enable-languages=ada,c,c++,d,fortran,lto,m2,objc,obj-c++,rust --prefix=/opt/skyhammer/x86_64-w64-mingw32 --enable-shared --enable-static --with-sysroot=/opt/skyhammer/x86_64-w64-mingw32 PATH=/opt/skyhammer/x86_64-w64-mingw32:/opt/skyhammer/aarch64-unknown-linux-gnu/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin";
+static const char thread_model[] = "win32";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
