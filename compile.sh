e49456429721c8b4722f2575ceb3eb2fce6869a0
Jessie Kirk <thejessiekirk@proton.me>
2024-09-08 17:34:38 +0000

    Improved compile script.

--- compile.sh
+++ compile.sh
@@ -14,6 +14,10 @@ if [ ! -d "compiled_that" ]; then
   echo "compiled_that does not exist. Skyhammer will create it.";
   mkdir compiled_that;
 fi;
+if [ ! -d "compiled_that/bin" ]; then
+  echo "compiled_that/bin does not exist. Skyhammer will create it.";
+  mkdir compiled_that/bin;
+fi;
 if [ -z "$( ls -A 'compile_this' )" ]; then
   echo "compile_this is empty, so there's nothing to do.";
   exit;
@@ -22,4 +26,5 @@ fi;
 echo "Compiling main.cxx...";
 export PATH=/opt/skyhammer/x86_64-w64-mingw32/bin:$OLD_PATH;
 cd compiled_that;
-x86_64-w64-mingw32-g++ ../compile_this/main.cxx -o main.exe;
+x86_64-w64-mingw32-g++ -m64 ../compile_this/main.cxx -o bin/main.exe;
+x86_64-w64-mingw32-strip bin/main.exe;
